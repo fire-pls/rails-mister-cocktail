31151f31613436416a4a702434ebbae60c2c085b
trevor malard <redsky.dz@gmail.com>
2017-10-12 14:35:43 +0900

    created doses controller

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,42 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.find(params[:id])
+    @dose.update(dose_params)
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails do
-    resources :doses, only: [ :new, :create ]
+    resources :doses, only: [ :new, :edit, :create, :update ]
   end
- resources :doses, only: [ :edit, :update, :destroy ]
+ resources :doses, only: [ :destroy ]
 end
