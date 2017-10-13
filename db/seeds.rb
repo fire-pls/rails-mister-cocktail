47c90fae8eb50b32ff876acf9c0e6c90dd305d6c
trevor malard <redsky.dz@gmail.com>
2017-10-13 15:20:48 +0900

    Real seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+require 'faker'
+
 if Ingredient.first.nil?
-  3.times do |_|
-    Ingredient.create(name:"ingredient#{_ + rand(1000)}")
+  json_list = JSON.parse(open("http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+  json_list.drinks.each do ingredient_id
+    Ingredient.create(name:"#{ingredient_id.strIngredient1}")
   end
 end
 
 
 10.times do |_|
-  cocktail = Cocktail.new(name:"cocktail#{_ + rand(1000)}")
+  cocktail = Cocktail.new(name:"#{Faker::Hipster.sentence(3)}")
   cocktail.photo_url = ("http://lorempixel.com/400/300/")
   cocktail.save
   ingredient = Ingredient.all.sample
-  dose = Dose.new(description: "seed dose")
+  dose = Dose.new(description: "#{rand(100)}mL")
   dose.ingredient = ingredient
   dose.cocktail = cocktail
   dose.save
